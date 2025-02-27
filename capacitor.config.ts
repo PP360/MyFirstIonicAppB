dc7a3e8398766a2b138651dad81a2fe41fd12417
José ValenciaCruz <josevalenciacruz@hotmail.com>
2025-02-27 21:41:37 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyFirstIonicAppB',
+  webDir: 'www'
+};
+
+export default config;
